c0fa7d030c3143e131d12ae0ed659a9fb4a03356
Travis <tscholl2@uw.edu>
2015-06-13 10:34:48 -0700

    initializing commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # beek
 Peek into binary files
+
+A command line utility that can peek into binary files and display values in multiple formats. Useful to see if a file containing a bit stream is all 0's or something.
+
+    NAME:
+       beek - Peek at binary files.
+
+    EXAMPLES:
+        # read first 16 bytes of test.bin in hex format
+        beek test.bin --format x -l 16
+    	# read last 8 bytes of test.bin in binary format
+    	beek --format b test.bin
+
+    USAGE:
+       beek [global options] command [command options] [arguments...]
+
+    VERSION:
+       0.0.0
+
+    COMMANDS:
+       help, h	Shows a list of commands or help for one command
+
+    GLOBAL OPTIONS:
+       --length, -l "8"	Number of bytes to read from file. Default: 8.
+       --format, -s "d"	Format of output:
+                'x' : hex
+                'X' : hex with capitol letters
+                'd' : base10
+                'a' : ascii
+       --delimiter, -d "\\n"    delimiter. Default is newline character
+       --offset, -o "0"	offset of reading in file. 0 for head, 10 for starting with 10th byte. offset of -10 reads last 10 bytes starting at -10,-9,-8,...
+       --help, -h		show help
+       --version, -v	print the version
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "beek"
+	app.Usage = `Peek at binary files.
+
+EXAMPLES:
+    # read first 16 bytes of test.bin in hex format
+    beek test.bin --format x -l 16
+	# read last 8 bytes of test.bin in binary format
+	beek --format b test.bin`
+
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "length, l",
+			Value: 8,
+			Usage: "Number of bytes to read from file. Default: 8.",
+		},
+		cli.StringFlag{
+			Name:  "format, s",
+			Value: "d",
+			Usage: `Format of output:
+            'x' : hex
+            'X' : hex with capitol letters
+            'd' : base10
+            'a' : ascii`,
+		},
+		cli.StringFlag{
+			Name:  "delimiter, d",
+			Value: "",
+			Usage: "delimiter. Default is newline character",
+		},
+		cli.IntFlag{
+			Name:  "offset, o",
+			Value: 0,
+			Usage: "offset of reading in file. 0 for head, 10 for starting with 10th byte. offset of -10 reads last 10 bytes starting at -10,-9,-8,...",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		arr := readFile(c.Args().First(), int64(c.Int("offset")), c.Int("length"))
+		var s string
+		for i, b := range arr {
+			s += fmt.Sprintf(fmt.Sprintf("%"+c.String("format"), b))
+			if i < len(arr)-1 {
+				if c.String("delimiter") == "" {
+					s += "\n"
+				} else {
+					s += c.String("delimiter")
+				}
+			}
+		}
+		fmt.Println(s)
+	}
+
+	app.Run(os.Args)
+}
+
+func readFile(name string, offset int64, size int) []byte {
+	f, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	if size < 0 {
+		log.Fatal("Size must not be out of Range")
+	}
+	buf := make([]byte, size)
+	if offset < 0 {
+		info, err := f.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		offset = info.Size() + offset
+	}
+	_, err = f.ReadAt(buf, offset)
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+	return buf
+}
